ea65d7bb06d73f953c79142f24764568f66d8bbb
negitoyo <negishitoyokazu@toyo-pc-2.local>
2019-05-12 14:23:06 +0900

    fix:herokuのroot

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
+Route::get('/', function () {
+    return redirect('/question');
+});
 
 Route::group(['prefix' => 'question'], function () {
     //toppage
