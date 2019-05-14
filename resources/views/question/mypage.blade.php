b5560645e06f7516615637a0c5e5877368843310
松田淳司 <amatsuda@zaico.co.jp>
2019-05-14 16:21:20 +0900

    Revert "CSSを呼ばない"
    This reverts commit 4986f21079da48431a1fb606439d9da5a8d658b8.

--- resources/views/question/mypage.blade.php
+++ resources/views/question/mypage.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.layout')
 @section('title', '農家に聞こう')
 @section('content')
+<link href="{{ asset('css/mypage.css') }}" rel="stylesheet">
 
   <h2 class="my-3">マイページ</h2>
   <div class="col-md-7 my-4">
