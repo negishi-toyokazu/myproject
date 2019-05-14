76334ee1d9fb2beef12d6ac27fcd7ebd331832e4
松田淳司 <amatsuda@zaico.co.jp>
2019-05-14 16:31:30 +0900

    Revert "一時的に自己紹介を非表示に"
    This reverts commit a404747695c1b691660bacbc463b62b9a912e31a.

--- resources/views/question/mypage.blade.php
+++ resources/views/question/mypage.blade.php
@@ -5,6 +5,25 @@
 
   <h2 class="my-3">マイページ</h2>
   <div class="col-md-7 my-4">
+    <div class="card p-3 bg-white">
+      @if($user->image_path == NULL)
+        <img class="rounded-circle　p-2　img-fluid img-thumbnail" height="200px" src="{{ asset('image/default_user.jpeg') }}" alt="プロフィール画像">
+      @else
+        <img class="rounded-circle p-2 img-fluid img-thumbnail"　width="100%" height="200px" src="{{ $user->image_path }}" alt="プロフィール画像">
+      @endif
+        <div class="content mt-3">
+          <h3 class="text-center text-bold text-large">{{$user->name}}</h3>
+        </div>
+        <hr>
+        <div class="content p-2">
+          <h4>自己紹介</h4>
+          <p class="m-2">{{$user->introduction}}</p>
+        </div>
+        <hr>
+        <div class="content">
+          <p class="text-center"><a class="btn btn-primary" href="{{route('edit',[$user->id])}}">編集する</a></p>
+        </div>
+    </div>
   </div>
   {{--検索フォーム--}}
   <div class="col-md-4">
