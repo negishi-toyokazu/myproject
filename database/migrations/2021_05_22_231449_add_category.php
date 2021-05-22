aa057e4e5bff0218297bd9308e37f603c7c29452
negishi-toyokazu <nnnnnttttt5@gmail.com>
2021-05-22 23:18:11 +0900

    category追加

---
+++ database/migrations/2021_05_22_231449_add_category.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCategory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            DB::table('users')->insert(
+                array(
+                    ['category' => 'きゅうり',
+                    'class' => '野菜'],
+                    ['category' => 'なす',
+                    'class' => '野菜']
+                    ['category' => 'みかん',
+                    'class' => '果物']
+                    ['category' => 'りんご',
+                    'class' => '果物']
+                )
+            );
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            //
+        });
+    }
+}
